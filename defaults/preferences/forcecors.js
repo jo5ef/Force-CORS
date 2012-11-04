2502c3fa18f20e671b4db539d7a096a38442fefc
lissyx <lissyx+github@lissyx.dyndns.org>
2012-11-04 16:53:08 +0100

    Change headers storage
    Splitting on ':' and ' ' avoids settings a 'Access-Control-Allow-Origin'
    header, which is necessary when dealing with Cookies. Changing the logic
    so that we will split on '|' for the headers and we will split in ' '
    for each header name and value.

--- defaults/preferences/forcecors.js
+++ defaults/preferences/forcecors.js
@@ -1,1 +1,1 @@
-pref('forcecors.headers', 'Access-Control-Allow-Origin:* Access-Control-Allow-Methods:POST,GET');
+pref('forcecors.headers', 'Access-Control-Allow-Credentials true|Access-Control-Allow-Origin http://127.0.0.1:8000|Access-Control-Allow-Methods POST,GET');
